c2cb49fbbcae0169b2a776f0d059aa2622ffddb9
lyne cha <lynecha@berkeley.edu>
2022-03-15 16:48:04 -0700

    successfully requested from giphy

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+"use strict";
+
 console.log("Let's get this party started!");
+
+/** use axios.get to make a request and the params would be the query string
+ * which would come from the form input on the html page
+ */
+ let $searchInput;
+ let $gifContainer = $("#gifs");
+ let $form = $("#form");
+ let $submitButton = $("#submitButton");
+ $form.on("submit", function (evt) {
+      evt.preventDefault();
+      console.log("im here");
+      $searchInput = $("#input").val();
+      return $searchInput;
+ });
+
+async function giphyRequest($searchInput) {
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: $searchInput, api_key: "F5CYNIl0M6UIubfF985JaQpAJLQ6uzMt"}  });
+    console.log(response);
+    return response.data;
+}
+
+function
