3b7c91518482b1ea4e20d44bdaf3d9f9396cf2f4
Melanie Wong <wong.mela@gmail.com>
2022-03-15 17:57:42 -0700

    finished giphy exercise except for remove button

--- app.js
+++ app.js
@@ -9,17 +9,36 @@ console.log("Let's get this party started!");
  let $gifContainer = $("#gifs");
  let $form = $("#form");
  let $submitButton = $("#submitButton");
- $form.on("submit", function (evt) {
+
+ /** Conductor function on the event listener. When user submits the form, it will
+  * call the giphyRequest and appendGif functions.
+  */
+ $form.on("submit", async function (evt) {
       evt.preventDefault();
       console.log("im here");
       $searchInput = $("#input").val();
-      return $searchInput;
+      let generatedGif = await giphyRequest($searchInput);
+      appendGif(generatedGif);
+
  });
 
+ /**Submitted our request to GIPHY and generating a random response to the input. */
 async function giphyRequest($searchInput) {
-    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: $searchInput, api_key: "F5CYNIl0M6UIubfF985JaQpAJLQ6uzMt"}  });
-    console.log(response);
-    return response.data;
+    let response = await axios.get("http://api.giphy.com/v1/gifs/search", { params: { q: $searchInput, api_key: "0769YYRyMYdbiaT3p8RRAzg0Ls9d8Wq3"}  });
+    //console.log(response);
+    console.log("response data =", response.data);
+    let randomImage = generateRandomImage(response.data.data);
+    return response.data.data[randomImage].images.original.url;
 }
 
-function
+/**Append the GIF to the DOM */
+function appendGif (generatedGif){
+    let $gifMain = $(`<img src = ${generatedGif}>`);
+    $gifContainer.append($gifMain);
+
+}
+
+/**Generate a random image based on the length of the API array. */
+function generateRandomImage (imageArray){
+   return Math.floor(Math.random() * imageArray.length);
+}
